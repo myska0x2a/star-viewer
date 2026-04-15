ff357f172a3756c91f56a3d3ef5819ad5aac29c5
Obi O'Brien <spaceman385@proton.me>
2026-04-15 14:53:30 +1000

    Set dev build optimisation to the same as release

--- build.rs
+++ build.rs
@@ -6,25 +6,25 @@ use std::path::Path;
 use std::io::{self, Write};
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
+    // let out_dir = env::var("OUT_DIR").unwrap();
 
-    let glsl_output = Command::new("glslc").args(&["shaders/stars/stars.vert", "-o", "shaders/stars/stars.vert.spv"])
-                        .current_dir(&Path::new(&out_dir))
-                        .stdout(Stdio::piped())
-                        .output();
+    // let glsl_output = Command::new("glslc").args(&["shaders/stars/stars.vert", "-o", "shaders/stars/stars.vert.spv"])
+    //                     .current_dir(&Path::new(&out_dir))
+    //                     .stdout(Stdio::piped())
+    //                     .output();
 
-    let output = Command::new("ls")
-        .stdout(Stdio::piped())
-        .output()
-        .expect("Failed to execute command");
+    // let output = Command::new("ls")
+    //     .stdout(Stdio::piped())
+    //     .output()
+    //     .expect("Failed to execute command");
 
      
 
-    let msg = String::from_utf8(glsl_output.unwrap().stdout).unwrap();
-    let msg2 = String::from_utf8_lossy(&output.stderr);
+    // let msg = String::from_utf8(glsl_output.unwrap().stdout).unwrap();
+    // let msg2 = String::from_utf8_lossy(&output.stderr);
 
 
-    println!("cargo::error=\"{}\"", msg2);
-    print!("");
+    // println!("cargo::error=\"{}\"", msg2);
+    // print!("");
 
 }
